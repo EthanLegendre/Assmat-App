db8968e8775f84c425287da5474e9b26bfc947da
ethan legendre <ethan.legendre@epitech.eu>
2026-09-02 10:50:49 +0200

    feat: use index for rediections

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,26 @@
 import "@/global.css"
-import { Text, View } from "react-native";
-import { Link } from 'expo-router'
-import { SafeAreaView } from "react-native-safe-area-context";
- 
+import { router } from 'expo-router'
+import { useEffect, useState } from "react";
+import { supabase } from "@/lib/supabase";
+
 export default function App() {
-  return (
-    <SafeAreaView className="flex-1 bg-bg x-10">
-      <Link href={"/register"}>Register</Link>
-    </SafeAreaView>
-  );
+  const [checkingSession, setCheckingSession] = useState(true);
+
+  useEffect(() => {
+    async function checkSession() {
+      const { data: { session } } = await supabase.auth.getSession();
+
+      if (!session) {
+        router.replace("/login");
+        return;
+      } else {
+        router.replace("/homepage");
+        return;
+      }
+    }
+    checkSession();
+  }, []);
+  if (checkingSession) {
+    return null
+  }
 }
