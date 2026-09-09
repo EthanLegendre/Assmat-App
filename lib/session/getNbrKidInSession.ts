bdda0a85ddd2feff2a74b78543e6714c801ee781
ethan legendre <ethan.legendre@epitech.eu>
2026-09-09 10:31:10 +0200

    feat: lib: add getNbrKidInSession function

---
+++ lib/session/getNbrKidInSession.ts
@@ -0,0 +1,18 @@
+import { supabase } from "../supabase";
+
+export async function getNbrKidInSession(): Promise<{ count: number | null; error: string | null}> {
+    const { data: { user } } = await supabase.auth.getUser();
+    if (!user) {
+        return { count: null, error: "User not connected !"}
+    }
+
+    const { count, error } = await supabase
+        .from("session_garde")
+        .select("*, enfant!inner(*)", { count: "exact", head: true })
+        .eq("status", "en_cours")
+        .eq("enfant.assmat_id", user.id);
+    if (error) {
+        return { count: null, error: error.message };
+    }
+    return { count, error: null};
+}
