70e25dd7b204b9cd5f1141696f452bef4120ae7d
ethan legendre <ethan.legendre@epitech.eu>
2026-09-21 09:07:44 +0200

    feat: tabs: add redirection for assmat or parent

--- app/index.tsx
+++ app/index.tsx
@@ -1,26 +1,30 @@
 import "@/global.css"
 import { router } from 'expo-router'
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { supabase } from "@/lib/supabase";
+import { fetchUser } from "@/lib/utils/fetchUser";
 
 export default function App() {
-  const [checkingSession, setCheckingSession] = useState(true);
-
   useEffect(() => {
-    async function checkSession() {
+    async function routeUser() {
       const { data: { session } } = await supabase.auth.getSession();
-
       if (!session) {
         router.replace("/login");
         return;
-      } else {
+      }
+      const { data: assmat } = await fetchUser("assmat");
+      if (assmat) {
         router.replace("/homepage");
         return;
       }
+      const { data: parent } = await fetchUser("parent");
+      if (parent) {
+        router.replace("/parent_homepage");
+        return;
+      }
+      router.replace("/login");
     }
-    checkSession();
+    routeUser();
   }, []);
-  if (checkingSession) {
-    return null
-  }
+  return null;
 }
