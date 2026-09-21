d3a90899df8100fee0d49799e643eb470ac21238
ethan legendre <ethan.legendre@epitech.eu>
2026-09-21 09:07:05 +0200

    feat: layout: add font

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,21 @@
 import { Stack } from "expo-router";
 import '@/global.css'
+import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold, Inter_900Black } from '@expo-google-fonts/inter';
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold,
+    Inter_900Black,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <Stack screenOptions={{ headerShown: false, animation: "none"}}>
-      
-    </Stack>
-  )
+    <Stack screenOptions={{ headerShown: false }} />
+  );
 }
