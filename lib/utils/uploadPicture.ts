17d03d8ff204068b914864fb01537effd93af472
ethan legendre <ethan.legendre@epitech.eu>
2026-09-21 09:15:54 +0200

    feat: lib: fix media permissions

--- lib/utils/uploadPicture.ts
+++ lib/utils/uploadPicture.ts
@@ -1,37 +1,42 @@
 import * as ImagePicker from "expo-image-picker";
+import * as FileSystem from "expo-file-system/legacy";
+import { decode } from "base64-arraybuffer";
 import { supabase } from "@/lib/supabase";
-import * as FileSystem from "expo-file-system/legacy"
 
-export async function pickAndUploadPicture(userId: string): Promise<{ url: string | null; error: string | null}> {
-    const { status } = await ImagePicker.requestCameraPermissionsAsync();
+export async function pickAndUploadPicture(userId: string): Promise<{ url: string | null; error: string | null }> {
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
 
     if (status !== "granted") {
-        return { url: null, error: "no permission"};
+        return { url: null, error: "no permission" };
     }
 
     const result = await ImagePicker.launchImageLibraryAsync({
         mediaTypes: ["images"],
         quality: 0.7,
         allowsEditing: true,
-        aspect: [1, 1]
+        aspect: [1, 1],
     });
 
     if (result.canceled) {
-        return {url: null, error: null};
+        return { url: null, error: null };
     }
 
     const uri = result.assets[0].uri;
 
-    const response = await fetch(uri);
-    const arrayBuffer = await response.arrayBuffer();
+    const base64 = await FileSystem.readAsStringAsync(uri, {
+        encoding: FileSystem.EncodingType.Base64,
+    });
+    const arrayBuffer = decode(base64);
 
     const filename = `${userId}/${Date.now()}.jpg`;
-    const { error: uploadError } = await supabase.storage.from("pictures").upload(filename, arrayBuffer, { contentType: "image/jpeg" });
+    const { error: uploadError } = await supabase.storage
+        .from("pictures")
+        .upload(filename, arrayBuffer, { contentType: "image/jpeg" });
 
     if (uploadError) {
-        return { url: null, error: uploadError.message};
+        return { url: null, error: uploadError.message };
     }
 
     const { data } = supabase.storage.from("pictures").getPublicUrl(filename);
-    return { url: data.publicUrl, error: null};
+    return { url: data.publicUrl, error: null };
 }
