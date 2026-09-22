728147672ecdd2262e9018f56b44259ec44314e1
ethan legendre <ethan.legendre@epitech.eu>
2026-09-22 16:14:39 +0200

    feat: components: change design homepage calendar

--- components/homepage/homepageCalendar.tsx
+++ components/homepage/homepageCalendar.tsx
@@ -4,7 +4,9 @@ import { fetchWeekKidPlanning } from "@/lib/utils/fetchWeekKidPlanning";
 import { useEffect, useState } from "react";
 import { PlannedChild } from "@/types/enfant";
 import { PlanningDay } from "@/types/planning";
-
+import { Ionicons } from "@expo/vector-icons";
+import { Pressable } from "react-native";
+import { router } from "expo-router";
 
 export function groupPlanningByDay(entries: PlanningDay[], week: Date[]) {
   return week.map((date) => {
@@ -38,46 +40,57 @@ export function HomepageCalendar() {
   const planningParJour = weekKid ? groupPlanningByDay(weekKid, week) : [];
 
   return (
-    <View className="w-full bg-white mt-8 shadow-lg rounded-[10px] flex-row justify-between px-4 py-4">
-      {planningParJour.map((jour, index) => {
-        const nom = jour.date.toLocaleDateString("fr-FR", { weekday: "short" });
-        const numero = jour.date.getDate();
-        const estAujourdhui = jour.date.toDateString() === new Date().toDateString();
-
-        return (
-          <View key={index} className="items-center gap-1.5">
-            <Text className={`text-[10.5px] font-semibold ${estAujourdhui ? "text-violet-deep" : "text-ink-faint"}`}>
-              {nom}
-            </Text>
-            <View className={`w-8 h-8 rounded-full items-center justify-center ${estAujourdhui ? "bg-violet" : ""}`}>
-              <Text className={`text-[13px] font-bold ${estAujourdhui ? "text-white" : "text-ink-soft"}`}>
-                {numero}
-              </Text>
-            </View>
-            <View className="flex-column -space-x-1">
-              {jour.child.map((child) => (
-                !child.profilePictureUrl ? (
-                  <View
-                    key={child.id}
-                    className="w-[12px] h-[12px] rounded-full items-center justify-center"
-                    style={{ backgroundColor: child.avatarColor }}
-                  >
-                    <Text className=" text-[5px] text-white font-extrabold">
-                      {child.firstname.charAt(0)}
-                    </Text>
-                  </View>
-                ) : (
-                  <Image
-                    key={child.id}
-                    source={{ uri: child.profilePictureUrl }}
-                    className="h-[12px] w-[12px] rounded-full"
-                  />
-                )
-              ))}
-            </View>
+    <View className="w-full bg-white mt-8 shadow-lg rounded-[10px] justify-between px-4 py-4">
+        <View className="flex-row justify-between items-center">
+          <View>
+            <Text className="font-inter-bold text-[18px]">Planning</Text>
+            <Text className="font-inter-semibold text-ink-faint text-[12px]">Cette semaine</Text>
           </View>
-        );
-      })}
+          <Pressable onPress={() => router.replace("/planning")}>
+            <Ionicons name="chevron-forward-outline" size={17}></Ionicons>
+          </Pressable>
+        </View>
+      <View className="flex-row justify-between mt-5">
+
+        {planningParJour.map((jour, index) => {
+          const nom = jour.date.toLocaleDateString("fr-FR", { weekday: "short" });
+          const numero = jour.date.getDate();
+          const estAujourdhui = jour.date.toDateString() === new Date().toDateString();
+          return (
+            <View key={index} className="items-center gap-1.5">
+              <Text className={`text-[10.5px] font-inter-semibold ${estAujourdhui ? "text-violet-deep" : "text-ink-faint"}`}>
+                {nom}
+              </Text>
+              <View className={`w-8 h-8 rounded-full items-center justify-center ${estAujourdhui ? "bg-violet/30" : ""}`}>
+                <Text className={`text-[13px] font-inter-bold ${estAujourdhui ? "text-white" : "text-ink-soft"}`}>
+                  {numero}
+                </Text>
+              </View>
+              <View className="flex-column -space-x-1">
+                {jour.child.map((child) => (
+                  !child.profilePictureUrl ? (
+                    <View
+                      key={child.id}
+                      className="w-[12px] h-[12px] rounded-full items-center justify-center"
+                      style={{ backgroundColor: child.avatarColor }}
+                    >
+                      <Text className=" text-[5px] text-white font-extrabold">
+                        {child.firstname.charAt(0)}
+                      </Text>
+                    </View>
+                  ) : (
+                    <Image
+                      key={child.id}
+                      source={{ uri: child.profilePictureUrl }}
+                      className="h-[12px] w-[12px] rounded-full"
+                    />
+                  )
+                ))}
+              </View>
+            </View>
+          );
+        })}
+      </View>
     </View>
   );
 }
